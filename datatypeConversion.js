70d4bd1884c2be95da7e6deaea5d2008683cf4e1
Tapas Chandra Das <tdas2802@gmail.com>
2025-08-07 18:18:18 +0600

    lession data type conversion

---
+++ datatypeConversion.js
@@ -0,0 +1,16 @@
+// let score = undefined;
+// console.log(typeof score); // "undefined"
+
+// let scoreString = String(score); //convert undefined to string
+// console.log(typeof scoreString); // "string"
+// console.log(scoreString); // "undefined"
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn); // Convert number to boolean
+//console.log(typeof booleanIsLoggedIn); // "boolean"
+console.log(booleanIsLoggedIn);
+
+
+
+//to convert a veriable to other data type there are three methods: String(), Number(), and Boolean()
+
